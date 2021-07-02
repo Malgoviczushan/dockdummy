6b7ba6954e3723ce491aace423dbaa51dd7e26aa
Malgoviczushan <86560352+Malgoviczushan@users.noreply.github.com>
2021-07-02 07:04:55 +0700

    Update Build.sh

--- scripts/Build.sh
+++ scripts/Build.sh
@@ -1,7 +1,7 @@
 echo "==========================="
 echo "ini isi dari file Build.sh"
-apt-get update
-apt-get install wget -y
-apt-get install openjdk-11-jdk -y
+#apt-get update
+#apt-get install wget -y
+#apt-get install openjdk-11-jdk -y
 ls
 echo "°°°°°°°°°°°°°°°°°°°°°°°°°°°"
